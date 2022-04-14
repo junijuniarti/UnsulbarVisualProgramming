422d7776c459d8cda21bde43b6dfef264adb2b11
junijuniarti <101160827+junijuniarti@users.noreply.github.com>
2022-04-14 12:38:13 +0800

    Add files via upload
    belajar pemrograman visual

---
+++ class_variable_juniarti.py
@@ -0,0 +1,24 @@
+#JUNIARTI_D0219908
+
+class Manusia:
+    nama : None
+    nim : None
+
+manusiaRakus = Manusia()
+manusiaRakus.nama = "jean"
+manusiaRakus.nim = "D0219028"
+
+manusiaManis = Manusia()
+manusiaManis.nama = "juniarti"
+manusiaManis.nim = "D0219008"
+
+manusiaCuek = Manusia()
+manusiaCuek .nama = "ayub"
+manusiaCuek .nim = "D0290888"
+
+print("manusiaRakus : \n"+manusiaRakus.nama, "\n"+manusiaRakus.nim)
+print("==========================")
+print("Manusia Manis: \n"+manusiaManis.nama, "\n"+manusiaManis.nim)
+print("==========================")
+print("manusiaCuek  : \n"+manusiaCuek.nama, "\n"+manusiaCuek.nim)
+print("\n \n \n TERIMAKASIH")
