8e4154ec13cece60f0aebe3eac77a0cfaa3bdd7d
junijuniarti <101160827+junijuniarti@users.noreply.github.com>
2022-04-20 16:36:12 +0800

    Add files via upload
    praktek matkul pemrograman visual

---
+++ latihan praktek object1_juniarti.py
@@ -0,0 +1,18 @@
+#JUNIARTI_D0219008
+
+class kucing:
+    pass
+
+kucing1 = kucing()
+kucing2 = kucing()
+
+kucing1.nama = "samoli"
+kucing1.warna = "coklat"
+kucing1.banyak = "2 ekor"
+
+kucing2.nama = "persians"
+kucing2.warna = "putih"
+kucing2.banyak = "5 ekor"
+
+print(kucing1.__dict__)
+
